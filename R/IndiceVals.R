a9d7064da405cafe4787c290de9fb4a6387e2a42
José Joaquín Atria <jjatria@gmail.com>
2017-10-22 12:50:39 +0100

    Add skeleton of dataset structure

---
+++ R/IndiceVals.R
@@ -0,0 +1,11 @@
+#' Physico-physico-chemical properties of 20 amino-acids
+#'
+#' This dataset contains 420 physico-physico-chemical properties
+#' of 20 amino-acids.
+#'
+#' @format A data frame with 20 rows and 420 variables. Each row
+#' contains data for a single amino-acid, and is named following
+#' the IUPAC naming standard. Each column contains data for one
+#' of 420 properties.
+#' @source \url{http://pbil.univ-lyon1.fr/members/lobry/repro/cabios95}
+"IndiceVals"
---
+++ R/ProtComp.R
@@ -0,0 +1,11 @@
+#' Amino-acid composition of E. coli proteins
+#'
+#' This dataset contains the amino-acid composition of 999 E. coli
+#' proteins.
+#'
+#' @format A data frame with 20 rows and 999 variables. Each row
+#' contains data for a single amino-acid, and is named following
+#' the IUPAC naming standard. Each column contains data for one
+#' E. coli protein.
+#' @source \url{http://pbil.univ-lyon1.fr/members/lobry/repro/cabios95}
+"ProtComp"
