a8d18f256eaf0ea1300a5dfaa14b9022d7e075c9
timschoof <timschoof@hotmail.com>
2017-10-22 13:07:15 +0100

    Create function computations.R and add documentation

--- R/computations.R
+++ R/computations.R
@@ -1,16 +1,24 @@
-#
-# Computations:
-#
-library(ade4)
-#
-# Correspondence analysis:
-#
-coa <- dudi.coa(df = ProtComp, scannf = FALSE, nf = 3)
-#
-# Principal Component Analysis with coa's row weigth:
-#
-pca <- dudi.pca(df = IndiceVals, row.w = coa$lw, scannf = FALSE, nf = 3)
-#
-# Co-inertia Analysis:
-#
-cia <- coinertia(dudiX = pca, dudiY = coa, scannf = FALSE, nf = 3)
+#' Title Computations for CABIOS 1995 data 
+#'
+#' @param Protcomp 
+#' @param IndiceVals 
+#'
+#' @return Results of Correspondence analysis, Principal Components Analysis, and Co-inertia Analysis
+#' @export
+#'
+#' @examples
+#' computations(Protcomp,IndiceVals)
+
+computations <- function(Protcomp,IndiceVals){
+  # load required package
+  library(ade4)
+  
+  # Correspondence analysis:
+  coa <- dudi.coa(df = ProtComp, scannf = FALSE, nf = 3)
+  
+  # Principal Component Analysis with coa's row weigth:
+  pca <- dudi.pca(df = IndiceVals, row.w = coa$lw, scannf = FALSE, nf = 3)
+  
+  # Co-inertia Analysis:
+  cia <- coinertia(dudiX = pca, dudiY = coa, scannf = FALSE, nf = 3)
+}
