91c9855bc552721b08636747a1f3c73541e14d57
José Joaquín Atria <jjatria@gmail.com>
2017-10-24 14:17:21 +0100

    Add vertical space to computations

--- R/computations.R
+++ R/computations.R
@@ -15,11 +15,25 @@ computations <- function(ProtComp, IndiceVals) {
   library(ade4)
   
   # Correspondence analysis:
-  coa <- dudi.coa(df = ProtComp, scannf = FALSE, nf = 3)
+  coa <- dudi.coa(
+    df     = ProtComp,
+    scannf = FALSE,
+    nf     = 3
+  )
   
   # Principal Component Analysis with coa's row weigth:
-  pca <- dudi.pca(df = IndiceVals, row.w = coa$lw, scannf = FALSE, nf = 3)
+  pca <- dudi.pca(
+    df     = IndiceVals,
+    row.w  = coa$lw,
+    scannf = FALSE,
+    nf     = 3
+  )
   
   # Co-inertia Analysis:
-  cia <- coinertia(dudiX = pca, dudiY = coa, scannf = FALSE, nf = 3)
+  cia <- coinertia(
+    dudiX  = pca,
+    dudiY  = coa,
+    scannf = FALSE,
+    nf     = 3
+  )
 }
