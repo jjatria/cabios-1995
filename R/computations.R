c6ef2ce4e0a6d366387220a0fa264dff0d38ef9c
José Joaquín Atria <jjatria@gmail.com>
2017-10-24 14:26:20 +0100

    Remove trailing colons from comments in computations

--- R/computations.R
+++ R/computations.R
@@ -11,15 +11,15 @@
 #' 
 computations <- function(ProtComp, IndiceVals) {
   axes = 3;
-  
-  # Correspondence analysis:
+
+  # Correspondence analysis
   coa <- ade4::dudi.coa(
     df     = ProtComp,
     scannf = FALSE,
     nf     = axes
   );
   
-  # Principal Component Analysis with coa's row weigth:
+  # Principal Component Analysis with coa's row weigth
   pca <- ade4::dudi.pca(
     df     = IndiceVals,
     row.w  = coa$lw,
@@ -27,7 +27,7 @@ computations <- function(ProtComp, IndiceVals) {
     nf     = axes
   );
   
-  # Co-inertia Analysis:
+  # Co-inertia Analysis
   cia <- ade4::coinertia(
     dudiX  = pca,
     dudiY  = coa,
